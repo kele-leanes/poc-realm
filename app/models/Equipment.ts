88bea0d50da95a4b9f5b1319930774d6575738cb
Héctor De Diego <lecksfrawen@gmail.com>
2023-01-02 15:25:39 -0600

    chore: update to fix schema, appId and other config

--- app/models/Equipment.ts
+++ app/models/Equipment.ts
@@ -1,11 +1,11 @@
-import {Realm} from '@realm/react';
+import { Realm } from "@realm/react";
 
 // To use a class as a Realm object type in Typescript with the `@realm/babel-plugin` plugin,
 // simply define the properties on the class with the correct type and the plugin will convert
 // it to a Realm schema automatically.
 export class Equipment extends Realm.Object {
   _v!: number;
-  _id!: Realm.BSON.ObjectId;
+  _id: Realm.BSON.ObjectId = new Realm.BSON.ObjectId();
   account!: string;
   bow!: string;
   images!: string[];
@@ -13,21 +13,32 @@ export class Equipment extends Realm.Object {
   isDeleted!: boolean;
   model!: string;
   name!: string;
-  serial!:string;
+  serial!: string;
 
-  // static schema = {
-  //   name: 'equipment',
-  //   properties: {
-  //     __v: "int",
-  //     _id: "objectId",
-  //     account: "string",
-  //     bow: "string",
-  //     images: "array",
-  //     installationDate: "double",
-  //     isDeleted: "bool",
-  //     model: "string",
-  //     name: "string",
-  //     serial: "string"
-  //   }
-  // }
+  static primaryKey = "_id";
+
+  constructor(
+    realm: Realm,
+    v: number,
+    account: string,
+    bow: string,
+    images: string[],
+    installationDate: number,
+    isDeleted: boolean,
+    model: string,
+    name: string,
+    serial: string
+  ) {
+    super(realm, {
+      v: v,
+      account: account,
+      bow: bow,
+      images: images,
+      installationDate: installationDate,
+      isDeleted: isDeleted,
+      model: model,
+      name: name,
+      serial: serial || "_SYNC_DISABLED_",
+    });
+  }
 }
